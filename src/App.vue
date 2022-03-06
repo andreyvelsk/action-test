f8bdf4b4c3777dae5ccf74298fef54fd65c3f9f3
andrey_velsk <andrey_velsk@mail.ru>
2022-03-06 20:16:10 +0300

    add some styles

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,17 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <nav class="header">
+    <router-link :to="{ name: 'main' }" class="header-item">
+      Главная
+    </router-link>
+    <router-link :to="{ name: 'settings' }" class="header-item">
+      Настройки
+    </router-link>
   </nav>
-  <router-view />
+  <div class="container">
+    <router-view />
+  </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+@import "@/scss/main.scss";
 </style>
